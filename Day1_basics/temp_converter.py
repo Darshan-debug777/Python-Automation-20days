633d7e62de1a77c3da74941cb502bd4615a6945b
Darshan-debug777 <jaindarshan297@gmail.com>
2025-12-06 22:43:17 +0530

    Added temp_converter.py for Day 1

---
+++ Day1_basics/temp_converter.py
@@ -0,0 +1,4 @@
+Celcius = input("Enter temperature in Celsius: ")
+Celcius = float(Celcius)
+Fahrenheit = (Celcius * 9/5) + 32
+print(f"Temperature in Celcius: {Celcius}°C \nTemperature in Fahrenheit: {Fahrenheit}°F")
